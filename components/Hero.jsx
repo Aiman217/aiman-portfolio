d2bd3cd0375b727f904d4cf93d917cc149e10611
Muhammad Aiman Arif Bin Baharudin <77523218+Aiman217@users.noreply.github.com>
2022-11-29 02:27:53 +0800

    Hero update.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   return (
     <>
       <div className="hero min-h-screen text-gray-400">
         <div className="hero-content flex-col lg:flex-row">
-          <img
-            src="https://placeimg.com/260/400/arch"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
+          <img src="/profile.jpg" className="max-w-sm rounded-lg shadow-2xl" />
           <div>
-            <h1 className="text-5xl font-bold">
+            <h1 className="text-3xl text-center lg:text-5xl lg:text-start font-bold">
               <Typewriter
                 words={["Nice to Meet You!", "Learn More About Me!"]}
                 loop={5}
@@ -22,10 +19,11 @@ const Hero = () => {
                 delaySpeed={1000}
               />
             </h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <p className="text-lg text-center lg:text-2xl lg:text-start py-6">
+              I am a Fourth Year Computer Science student from Universiti Sains
+              Malaysia (USM). I am majoring in Intelligent Computing and
+              minoring in Management. Passion on web development using
+              frameworks like React and NextJS.
             </p>
           </div>
         </div>
