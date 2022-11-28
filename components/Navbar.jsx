aaef6241b50e4c4acee5d403893c3da98a771c0c
Muhammad Aiman Arif Bin Baharudin <77523218+Aiman217@users.noreply.github.com>
2022-11-29 02:13:30 +0800

    Navbar update.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiFillPhone, AiTwotoneMail } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -7,22 +8,19 @@ const Navbar = () => {
         <div className="flex-1">
           <a className="btn btn-ghost font-bold italic text-xl">Aiman Arif</a>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
+        <div className="flex-none gap-2">
+          <div className="badge h-full">
+            <AiFillPhone size={25} className="pr-2" />
+            +60-17 213 9693
+          </div>
+          <div className="badge h-full">
+            <AiTwotoneMail size={25} className="pr-2" />
+            aimanarif217@gmail.com
+          </div>
+          <div className="badge h-full">
+            <AiTwotoneMail size={25} className="pr-2" />
+            aimanarif217@student.usm.my
+          </div>
         </div>
       </div>
     </>
