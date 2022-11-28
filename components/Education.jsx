3981d04443715208c4cf88c313397c9958226a53
Muhammad Aiman Arif Bin Baharudin <77523218+Aiman217@users.noreply.github.com>
2022-11-29 03:07:29 +0800

    Education update.

--- components/Education.jsx
+++ components/Education.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const Education = () => {
+  var educationLists = [
+    {
+      status: "Finished",
+      place: "KOLEJ MATRIKULASI SELANGOR",
+      level: "Matriculation",
+      color: "badge-warning"
+    },
+    {
+      status: "Current",
+      place: "UNIVERSITI SAINS MALAYSIA",
+      level: "Bachelor of Computer Science (Honours)",
+      color: "badge-success"
+    },
+  ];
   return (
     <>
       <div className="card w-full bg-base-100 shadow-xl image-full">
@@ -8,10 +22,17 @@ const Education = () => {
           <img src="./education.jpg" alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <h2 className="card-title pb-4">My Education!</h2>
+          <div className="card-actions justify-start">
+            {educationLists.map((edu, index) => (
+              <div key={index}>
+                <div className="flex flex-row items-center gap-2">
+                  <div className={"badge h-full " + edu.color}>{edu.status}</div>
+                  <div className={"badge h-full " + edu.color}>{edu.level}</div>
+                </div>
+                <div className={"badge h-full " + edu.color}>{edu.place}</div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
